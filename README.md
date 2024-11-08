18b09619e52c05d896ce59f0eebd863efbfbef2f
Naurangi_lal <naurangilal15072002@gmail.com>
2024-11-09 01:12:56 +0530

    Add server client data

--- README.md
+++
@@ -1,1 +0,0 @@
-I initiate the project to clarify some concept of js with node.js with react.
--- routes/commentRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/commentRoutes.js
-const express = require('express');
-const router = express.Router();
-const { addComment } = require('../controllers/commentController');
-const {authMiddleware,generateToken} = require('../middleware/authMiddleware');
-
-router.post('/:postId', generateToken,authMiddleware, addComment);
-
-module.exports = router;
