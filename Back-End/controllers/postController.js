7aa65d6793814b8dce8bd89aebd06e501a496ef2
gituser123290 <naurangilal9675329115@gmail.com>
2025-02-13 21:14:49 +0530

    fixed backend

--- Back-End/controllers/postController.js
+++ Back-End/controllers/postController.js
@@ -32,7 +32,7 @@ exports.createPost = async (req, res) => {
 
 exports.getPosts = async (req, res) => {
   try {
-    const posts = await Post.find({})
+    const posts = await Post.find()
       .populate('user', 'username photo bio') 
       .populate({
         path: 'comments',
